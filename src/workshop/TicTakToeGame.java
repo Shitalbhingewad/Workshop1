54f7744c0d06fe0d6839e481ada0f1e67a17c668
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-10-18 11:15:43 +0530

    Created tictactoe empty board for tictactoegame

--- src/workshop/TicTakToeGame.java
+++ src/workshop/TicTakToeGame.java
@@ -1,8 +1,19 @@
 package workshop;
 
 public class TicTakToeGame {
+	char[] board;
+
+		public void createboard() {//Create empty Board With empty values
+			char[] board = new char[10];
+			for (int i = 1; i < board.length; i++) {
+				board[i] = ' ';
+
+			}
+		}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to workshop usecases");
+		TicTakToeGame obj = new TicTakToeGame();
+		obj.createboard();
 	}
 }
